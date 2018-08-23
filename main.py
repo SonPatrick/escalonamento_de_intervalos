72ad12f2b7823ed4743d87c2f57df8e111c18323
Hugo Victor dos Santos Silva <hugovictorsilva94@gmail.com>
2018-08-23 17:57:30 -0300

    atualização do código

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from esca_inter import escalonamento_intervalor
+from esca_inter import escalona
 
 s =[0, 2, 3, 3, 5, 5, 6, 7, 8, 1]
 f =[4, 4, 5, 6, 6, 7, 7, 9, 10, 2]
 
-escalonamento_intervalor(s, f)
+escalona(s, f)
